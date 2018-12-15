4c27a966d03819f766c10c85ddd53fdce1476822
UZziel <aliafifmm@gmail.com>
2018-12-15 17:42:48 +0330

    gui almost completed

--- gui.py
+++ gui.py
@@ -16,39 +16,68 @@ f3 = Frame(root)
 
 for frame in (f1, f2, f3):
     frame.grid(row=0, column=0, sticky='news')
-
+#
+#
+# Frame 1
 photo = PhotoImage(file="espresso2.png")
-
 Label(f1, image=photo).place(x=0, y=0, relwidth=1, relheight=1)
 Button(f1, text=' Single', bg="black", fg="white", command=lambda: raise_frame(f2)).place(x=20, y=40)
 Button(f1, text='Double', bg="black", fg="white", command=lambda: raise_frame(f3)).place(x=20, y=80)
 Label(f1, text="Hint\n"
                "Single: estimates an algorithm runtime\n"
                "Double: compares two algorithms runtimes", fg="white", bg="black").place(x=20, y=680)
+#
+#
+#
 
 # FRAME 2 #
 # Label(f2, text='FRAME 2').pack()
+# lbl = Label(f2, text="")
+# lbl.grid(column=0, row=0)
 Label(f2, image=photo).place(x=0, y=0, relwidth=1, relheight=1)
-lbl = Label(f2, text=" ")
-lbl.grid(column=0, row=0)
 
-hintlbl = Label(f2, text="Hint: Use python syntax")
+hintlbl = Label(f2, text="Hint: Paste code in the box", bg="black", fg="white")
 hintlbl.grid(column=3, row=0)
 
-txt = Text(f2, height=40, width=100)
-txt.grid(column=1, row=1)
+f2entry = Text(f2, bg="black", fg="white", height=40, width=100)
+# txt.insert(END, "paste your code here")
+f2entry.grid(column=1, row=1)
 
 
-def clicked():
-    print(txt.get("1.0", "end-1c"))
+def calchandle():
+    print(f2entry.get("1.0", "end-1c"))
 
 
-btn = Button(f2, text="Calculate", command=clicked)
-btn.grid(column=1, row=2)
-Button(f2, text='back', command=lambda: raise_frame(f1)).grid(row=10, column=10)
+btn = Button(f2, text="Calculate", bg="black", fg="white", command=calchandle)
+btn.grid(column=1, row=3)
+Button(f2, text='Back', bg="black", fg="white", command=lambda: raise_frame(f1)).grid(row=3, column=4)
+Label(f2, text="", bg="black", fg="white").grid(row=10, column=10)
+#
+#
+#
+#
+# Frame 3 #
+Label(f3, image=photo).place(x=0, y=0, relwidth=1, relheight=1)
+f3entry = Text(f3, bg="black", fg="white", height=40, width=50)
+# entry.insert(END, "paste your code here")
+f3entry.grid(column=1, row=1)
 
-# Label(f3, text='FRAME 3').pack(side='left')
-Button(f3, text='back', command=lambda: raise_frame(f1)).pack(side='left')
+lbl = Label(f3, bg="black", fg="white", text="")
+lbl.grid(column=2, row=1)
+
+f3entry2 = Text(f3, bg="black", fg="white", height=40, width=50)
+f3entry2.grid(column=3, row=1)
+
+
+def comparehandle():
+    print(f3entry.get("1.0", "end-1c"))
+    print("\n")
+    print(f3entry2.get("1.0", "end-1c"))
+
+
+btn = Button(f3, text="Compare", bg="black", fg="white", command=comparehandle)
+btn.grid(row=3, columnspan=4)
+Button(f3, text='back',bg="black", fg="white", command=lambda: raise_frame(f1)).place(x=970, y=690)
 
 raise_frame(f1)
 root.mainloop()
