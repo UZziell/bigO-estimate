d863dcf483ed82e6e311f3aa93cdb34f772f1ed4
uzziel <aliafifmm@gmail.com>
2018-12-27 20:24:14 +0330

    little changes in api

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import json
 class Api:
     def __init__(self, user_input):
         self.url = "https://api.jdoodle.com/v1/execute"
-        self.headers = {'Content-type': 'application/json'}
+        self.headers = {'Content-type': 'application//x-www-form-urlencoded'}
         self.payload = {"clientId": "348c57e0f1663952690f16dba2834798",
                         "clientSecret": "c6b227deeb8043545220457cd43ea3495d490fbd972acdc904df176864d37e4c",
                         "script": """{}""".format(user_input),
